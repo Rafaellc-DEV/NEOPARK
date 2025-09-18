05ce6f2049edc3c1be13d7c5c47c150795b1ed2d
Rafaellc-DEV <rafaelcosta.2600@gmail.com>
2025-09-17 21:04:52 -0300

    adicionando o método set da classe Veiculo

--- br.com.neopark/src/entities/Veiculo.java
+++ br.com.neopark/src/entities/Veiculo.java
@@ -13,12 +13,18 @@ public class Veiculo {
         this.tipo = tipo;
         this.modelo = modelo;
         this.cor = cor;
+        
     }
 
     public String getPlaca() { return placa; }
     public String getTipo() { return tipo; }
     public String getModelo() { return modelo; }
     public String getCor() { return cor; }
+
+    public void setPlaca(String placa) { this.placa = placa; }
+    public void setTipo(String  tipo) { this.tipo = tipo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+    public void setCor(String cor) { this.cor = cor; }
 
     @Override
     public String toString() {
